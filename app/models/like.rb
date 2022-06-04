203fdac4d3d16c17b6e5e8f6901de88cd535d4a4
carlos-osorio-developer <osorio.uis@gmail.com>
2022-06-04 16:25:46 -0500

    Create Likes table, relations and validations

---
+++ app/models/like.rb
@@ -0,0 +1,6 @@
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :joke
+
+  validates :user_id, uniqueness: { scope: :joke_id, message: "You can only add a joke as a favorite once" }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_04_151929) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_04_211636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_04_151929) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "joke_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["joke_id"], name: "index_likes_on_joke_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_04_151929) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "jokes"
+  add_foreign_key "likes", "users"
 end
