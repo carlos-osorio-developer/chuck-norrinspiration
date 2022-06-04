b86c864561c85a078cb0abe854bc10dbc15edef2
carlos-osorio-developer <osorio.uis@gmail.com>
2022-06-04 14:55:02 -0500

    Add new joke into local db using like button

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,4 +1,21 @@
 class JokesController < ApplicationController
   def index
   end
+
+  def create
+    @joke = Joke.new(joke_params)
+    respond_to do |format|
+      if @joke.save
+        flash[:notice] = "Joke was successfully created."
+      else
+        flash[:error] = 'Joke was not created.'
+      end
+    end
+  end
+
+  private
+
+  def joke_params
+    params.require(:joke).permit(:api_id, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'welcome/show'
-  get 'jokes/index'  
+  get 'welcome/show'    
+  resources :jokes, only: [:index, :create]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
