7ab5ca7b2de773b03dd7373f9750eed0b6c86b3d
carlos-osorio-developer <osorio.uis@gmail.com>
2022-06-05 16:44:31 -0500

    Refactor like creation

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -6,12 +6,19 @@ class JokesController < ApplicationController
   def create
     @joke = Joke.new(joke_params)
     if @joke.save
-      Like.create(joke_id: @joke.id, user_id: current_user.id)
-      flash.now[:success] = 'Joke was successfully created.'
+      add_like(@joke)      
     else
       @stored_joke = Joke.find_by(api_id: joke_params[:api_id])
-      Like.create(joke_id: @stored_joke.id, user_id: current_user.id)
-      flash.now[:error] = 'This joke already exists.'
+      add_like(@stored_joke)      
+    end    
+  end
+
+  def add_like(joke)
+    @like = Like.new(joke_id: joke.id, user_id: current_user.id)
+    if @like.save
+      flash.now[:success] = t('msgs.joke_added')
+    else
+      flash.now[:error] = t('msgs.duplicate_joke')
     end
     render_flash
   end
@@ -19,7 +26,7 @@ class JokesController < ApplicationController
   def destroy_like
     @like = Like.find_by(joke_id: params[:id], user_id: current_user.id)
     @like.destroy
-    flash.now[:success] = 'Joke successfully removed from favorites.'
+    flash.now[:success] = t('msgs.delete_joke')
     render_flash
   end
 
