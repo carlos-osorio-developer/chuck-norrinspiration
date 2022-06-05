36576500bf60394905931c8995d6703d6c669821
carlos-osorio-developer <osorio.uis@gmail.com>
2022-06-05 18:40:33 -0500

    Refactor integration tests to split features

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -4,21 +4,10 @@ RSpec.feature 'Welcome', type: :feature do
   before :each do
     DatabaseCleaner.clean
     user = FactoryBot.create(:user)
-    sign_in(user)    
+    sign_in(user)
   end
 
-  describe 'list of jokes' do    
-    it 'renders a list of jokes' do      
-      visit '/'
-      click_link 'Get a new joke'
-      find(:css, 'button.btn.btn-danger').click 
-      visit '/'
-      click_link 'My Jokes', match: :first
-      expect(page).to have_content('Joke #1')
-    end
-  end
-
-  describe 'click NewJoke button' do    
+  describe 'click NewJoke button' do
     it 'shows Chuck Norris joke' do
       visit '/'
       click_link 'Get a new joke'
