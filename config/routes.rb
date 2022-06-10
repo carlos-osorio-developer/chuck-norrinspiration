bc5459ad59960c38474ad47489cbcaafc58d73a3
carlos-osorio-developer <osorio.uis@gmail.com>
2022-06-09 21:52:52 -0500

    Add internationalization (i18n) toggle links

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get 'welcome/show'    
-  resources :jokes, only: [:index, :create]
-  #route to destroy a like
-  delete 'jokes/:id/like' => 'jokes#destroy_like', as: :destroy_like    
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root 'welcome#index'
+    get 'welcome/show'    
+    resources :jokes, only: [:index, :create]    
+    delete 'jokes/:id/like' => 'jokes#destroy_like', as: :destroy_like    
+    devise_for :users    
+  end
 end
